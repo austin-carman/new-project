def372620f37975532397bfc454a45b8634699dc
Austin Carman <abcarman12@gmail.com>
2022-03-08 12:03:06 -1000

    correct sizing of date fields and select fields

--- src/components/Home/Search.js
+++ src/components/Home/Search.js
@@ -1,9 +1,9 @@
-import { Card, Button, Form, Row, Col, FloatingLabel } from 'react-bootstrap';
+import { Card, Button, Form, Row, Col, FloatingLabel, InputGroup } from 'react-bootstrap';
 
 const Search = () => {
   return (
     <div>
-      <Card border="none">
+      <Card border="light">
         <Card.Body>
           <Card.Title>Find places to stay on Airbnb</Card.Title>
           <Card.Text>Discover entire homes and private rooms perfect for any trip.</Card.Text>
@@ -15,51 +15,53 @@ const Search = () => {
             </Form.Group>
 
             <Row>
-              <Form.Group as={Col}>
-                <FloatingLabel controlId="floatingTextarea" label="Check in" className="mb-3">
-                  <Form.Control type="date" />
-                </FloatingLabel>
-              </Form.Group>
-
-              <Form.Group as={Col}>
-                <FloatingLabel controlId="floatingTextarea" label="Check out" className="mb-3">
-                  <Form.Control type="date" />
-                </FloatingLabel>
-              </Form.Group>
+              <InputGroup>
+                <Form.Group as={Col}>
+                  <FloatingLabel controlId="floatingTextarea" label="Check in" className="mb-3">
+                    <Form.Control type="date" />
+                  </FloatingLabel>
+                </Form.Group>
+                <Form.Group as={Col}>
+                  <FloatingLabel controlId="floatingTextarea" label="Check out" className="mb-3">
+                    <Form.Control type="date" />
+                  </FloatingLabel>
+                </Form.Group>
+              </InputGroup>
             </Row>
 
             <Row className="mb-3">
-              <Form.Group as={Col}>
-                <Form.Select id="dropdown-basic-button" defaultValue="2">
-                  <option href="#/action-1">1</option>
-                  <option href="#/action-2">2</option>
-                  <option href="#/action-3">3</option>
-                  <option href="#/action-3">4</option>
-                  <option href="#/action-3">5</option>
-                  <option href="#/action-3">6</option>
-                  <option href="#/action-3">7</option>
-                  <option href="#/action-3">8</option>
-                  <option href="#/action-3">9</option>
-                  <option href="#/action-3">10</option>
-                  <option href="#/action-3">11</option>
-                  <option href="#/action-3">12</option>
-                  <option href="#/action-3">13</option>
-                  <option href="#/action-3">14</option>
-                  <option href="#/action-3">15</option>
-                  <option href="#/action-3">16</option>
-                </Form.Select>
-              </Form.Group>
-
-              <Form.Group as={Col}>
-                <Form.Select id="dropdown-basic-button" title="Children">
-                  <option href="#/action-1">1</option>
-                  <option href="#/action-2">2</option>
-                  <option href="#/action-3">3</option>
-                  <option href="#/action-3">4</option>
-                  <option href="#/action-3">5</option>
-                  <option href="#/action-3">6</option>
-                </Form.Select>
-              </Form.Group>
+              <InputGroup>
+                <Form.Group as={Col}>
+                  <Form.Select id="dropdown-basic-button" defaultValue="2">
+                    <option href="#/action-1">1</option>
+                    <option href="#/action-2">2</option>
+                    <option href="#/action-3">3</option>
+                    <option href="#/action-3">4</option>
+                    <option href="#/action-3">5</option>
+                    <option href="#/action-3">6</option>
+                    <option href="#/action-3">7</option>
+                    <option href="#/action-3">8</option>
+                    <option href="#/action-3">9</option>
+                    <option href="#/action-3">10</option>
+                    <option href="#/action-3">11</option>
+                    <option href="#/action-3">12</option>
+                    <option href="#/action-3">13</option>
+                    <option href="#/action-3">14</option>
+                    <option href="#/action-3">15</option>
+                    <option href="#/action-3">16</option>
+                  </Form.Select>
+                </Form.Group>
+                <Form.Group as={Col}>
+                  <Form.Select id="dropdown-basic-button" title="Children">
+                    <option href="#/action-1">1</option>
+                    <option href="#/action-2">2</option>
+                    <option href="#/action-3">3</option>
+                    <option href="#/action-3">4</option>
+                    <option href="#/action-3">5</option>
+                    <option href="#/action-3">6</option>
+                  </Form.Select>
+                </Form.Group>
+              </InputGroup>
             </Row>
 
             <div className="d-grid gap-2">
